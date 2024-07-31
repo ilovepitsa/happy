d25be6127552e5389afce4acde81a7177bb72992
ilovepitsa <serialch1ller1537@gmail.com>
2024-07-31 19:52:48 +0300

    first version

--- auth/cmd/main.go
+++ auth/cmd/main.go
@@ -1,7 +1,18 @@
 package main
 
-func main() {
-	if err := app.Run(); err != nil {
+import (
+	"flag"
+	"log"
 
+	"github.com/ilovepitsa/happy/auth/internal/app"
+)
+
+var (
+	configPath = flag.String("c", "config/config.yaml", "use for custom config")
+)
+
+func main() {
+	if err := app.Run(*configPath); err != nil {
+		log.Fatal(err)
 	}
 }
