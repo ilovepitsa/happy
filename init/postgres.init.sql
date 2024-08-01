836847da906ca3ab67fc611c1f623d992d95ac22
ilovepitsa <serialch1ller1537@gmail.com>
2024-08-01 16:20:56 +0300

    postgres init

--- init/postgres.init.sql
+++ init/postgres.init.sql
@@ -1,2 +1,18 @@
-drop table if exists user_follow;
-drop table if exists users;
+DROP TABLE IF EXISTS user_follow;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id serial,
+    last_name varchar(100) not null,
+    first_name varchar(100) not null,
+    email varchar(255) not null,
+    birthday date not null
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE user_follow (
+    subscriber_id integer references users(id),
+    user_id integer references users(id),
+    notify_before varchar(5)
+);
+
